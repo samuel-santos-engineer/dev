3172a37ba0e66a93928b0a17602e2d0bc0e90266
Samuel Santos <samuel.santos.engineer@gmail.com>
2026-07-15 02:22:55 -0300

    add BeautifulDaysattheMoviesResult

---
+++ docs/rebrand/HackerRank/easy/BeautifulDaysattheMoviesResult.cs
@@ -0,0 +1,67 @@
+// Beautiful Days at the Movies
+// https://www.hackerrank.com/challenges/beautiful-days-at-the-movies/problem
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class BeautifulDaysattheMoviesResult
+{
+
+    /*
+     * Complete the 'beautifulDays' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER i
+     *  2. INTEGER j
+     *  3. INTEGER k
+     */
+
+    public static int beautifulDays(int i, int j, int k)
+    {
+        int q = 0;
+        
+        for(int d = i; d <= j; d++)
+        {
+            int rev = int.Parse(new string(d.ToString().Reverse().ToArray()));
+            int dif = rev - d;
+            if (dif % k == 0) q++;
+        }
+        
+        return q;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+        int i = Convert.ToInt32(firstMultipleInput[0]);
+
+        int j = Convert.ToInt32(firstMultipleInput[1]);
+
+        int k = Convert.ToInt32(firstMultipleInput[2]);
+
+        int result = Result.beautifulDays(i, j, k);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
