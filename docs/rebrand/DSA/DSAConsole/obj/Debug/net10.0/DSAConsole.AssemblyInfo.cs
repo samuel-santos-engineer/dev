81a75857bda7842c0ac69bb3b337dab801a3a9f6
Samuel Santos <samuel.santos.engineer@gmail.com>
2026-08-06 09:27:46 -0300

    update auto generated .net files

--- docs/rebrand/DSA/DSAConsole/obj/Debug/net10.0/DSAConsole.AssemblyInfo.cs
+++ docs/rebrand/DSA/DSAConsole/obj/Debug/net10.0/DSAConsole.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DSAConsole")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11cc0857c1fe1e0d0c4dd24aae6ab9914fd75775")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+436e58b72634a8cf7978b24ff94c1545e8f040ef")]
 [assembly: System.Reflection.AssemblyProductAttribute("DSAConsole")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DSAConsole")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
