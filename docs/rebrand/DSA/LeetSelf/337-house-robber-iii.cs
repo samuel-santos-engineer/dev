154adbab6f1ffb7ac11adf58b4c9c18fc69ce707
Samuel Santos <samuel.santos.engineer@gmail.com>
2026-09-24 09:55:23 -0300

    add 337-house-robber-iii

---
+++ docs/rebrand/DSA/LeetSelf/337-house-robber-iii.cs
@@ -0,0 +1,56 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public int Rob(TreeNode root) {
+
+        var mem = new Dictionary<(TreeNode, bool), int>();
+        
+        int maxSum = Math.Max(
+            CheckHouse(root, true, mem),
+            CheckHouse(root, false, mem)
+        );
+        return maxSum;
+    } 
+
+    static int CheckHouse(TreeNode n, bool onNode, Dictionary<(TreeNode, bool), int> mem)
+    {
+        if (n == null) return 0;
+
+        var memKey = (n, onNode);
+
+        if (mem.TryGetValue(memKey, out int maxSum)) return maxSum;
+
+        maxSum = 0;
+
+        if (onNode)
+        {
+            maxSum =
+                n.val +
+                CheckHouse(n.left, false, mem) +
+                CheckHouse(n.right, false, mem);
+        }
+        else
+        {
+            int left = Math.Max(CheckHouse(n.left, false, mem), CheckHouse(n.left, true, mem));
+            
+            int right = Math.Max(CheckHouse(n.right, false, mem), CheckHouse(n.right, true, mem));
+
+            maxSum = left + right;
+        }
+
+        mem.Add(memKey, maxSum);
+        
+        return maxSum;
+    }
+}
